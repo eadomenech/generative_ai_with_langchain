5c6662baa2daa1dbac3d66c338eeb8faa39107d4
Ernesto Avila Domenech <eadomenech@gmail.com>
2024-05-01 13:58:11 -0400

    Working on 02_main

--- mytests/02_main.py
+++ mytests/02_main.py
@@ -1,7 +1,7 @@
 from langchain.agents import AgentType
 from langchain.agents import initialize_agent
 from langchain.agents import load_tools
-from langchain.llms import OpenAI
+from langchain_community.llms import OpenAI
 
 tools = load_tools(["python_repl"])
 llm = OpenAI(temperature=0., model="text-davinci-003")
